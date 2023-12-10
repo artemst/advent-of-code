031f0e60876bc596cea01e43b7f164e73e70becd
Artem Stepanov <artem.omsk@gmail.com>
2023-12-10 17:52:15 +0300

    Add solution for day 8 (2023)

---
+++ 2023/day-08/src/main.rs
@@ -0,0 +1,113 @@
+use core::panic;
+use std::{collections::HashMap, io};
+
+use num::integer::lcm;
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    let input_filename = if args.get(1).is_some_and(|arg| arg.starts_with("-d")) {
+        format!("input-test{}.txt", &args.get(1).unwrap()[2..])
+    } else {
+        "input.txt".to_string()
+    };
+
+    let file = std::fs::read_to_string(input_filename)?;
+    let (instructions, map) = file.split_once("\n\n").unwrap();
+    let instructions = Instructions::from(instructions);
+    let map = map
+        .trim()
+        .split('\n')
+        .map(|row| row.split_once(" = ").unwrap())
+        .map(|(left, right)| (left, right[1..9].split_once(", ").unwrap()))
+        .fold(HashMap::new(), |mut acc, (key, val)| {
+            acc.insert(key, val);
+            acc
+        });
+
+    if map.contains_key("AAA") {
+        let result_for_task_1 = get_value_for_task_1(&instructions, &map);
+        println!("[1] Result for task 1 = {result_for_task_1}");
+    }
+    let result_for_task_2 = get_value_for_task_2(&instructions, &map);
+    println!("[2] Result for task 2 = {result_for_task_2}");
+
+    Ok(())
+}
+
+fn get_value_for_task_1(instructions: &Instructions, map: &HashMap<&str, (&str, &str)>) -> usize {
+    let mut last_code = "AAA";
+    for (counter, i) in instructions.clone().enumerate() {
+        last_code = match i {
+            Direction::Left => map[last_code].0,
+            Direction::Right => map[last_code].1,
+        };
+        // println!("counter: {counter}; dir: {i:?}, code: {code}");
+        if last_code == "ZZZ" {
+            return counter + 1;
+        }
+    }
+    0
+}
+
+fn get_value_for_task_2(instructions: &Instructions, map: &HashMap<&str, (&str, &str)>) -> usize {
+    let starts: Vec<&str> = map.keys().cloned().filter(|k| k.ends_with('A')).collect();
+    starts
+        .iter()
+        .map(|&k| {
+            let mut last_code = k;
+            for (counter, i) in instructions.clone().enumerate() {
+                last_code = match i {
+                    Direction::Left => map[last_code].0,
+                    Direction::Right => map[last_code].1,
+                };
+                if last_code.ends_with('Z') {
+                    return counter + 1;
+                }
+            }
+            0
+        })
+        .fold(1, lcm) // calculate Least Common Multiply for all results
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Debug, Clone)]
+struct Instructions {
+    data: Vec<Direction>,
+    pos: usize,
+}
+
+impl Instructions {
+    fn from(str: &str) -> Self {
+        Self {
+            data: str
+                .trim()
+                .chars()
+                .map(|c| match c {
+                    'L' => Direction::Left,
+                    'R' => Direction::Right,
+                    dir => panic!("Unknown direction: {dir}"),
+                })
+                .collect(),
+            pos: 0,
+        }
+    }
+}
+
+impl Iterator for Instructions {
+    type Item = Direction;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let result = self.data[self.pos];
+        if self.pos + 1 == self.data.len() {
+            self.pos = 0;
+        } else {
+            self.pos += 1;
+        }
+        Some(result)
+    }
+}
